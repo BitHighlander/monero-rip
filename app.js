cf6f76e517541faa5e15b5240239338e2e4cd662
highlander <bithighlander@gmail.com>
2019-12-30 14:53:27 -0700

    cron

--- app.js
+++ app.js
@@ -4,28 +4,28 @@ let tools = require("./tools.js")
 
 
 
-tools.getBalance()
-    .then(function(resp){
-        console.log("resp: ",resp)
-    })
+// tools.getBalance()
+//     .then(function(resp){
+//         console.log("resp: ",resp)
+//     })
 
-// let debug = function(){
-//     let time = new Date()
-//     console.log("current time: ",time)
-//     console.log("current time: ",time.toLocaleString())
-// }
-// setInterval(debug,1000)
-//
-// // weekday run at noon
-// cron.schedule('0 12 * * 1-5', () => {
-//     run_audit()
-//     console.log("Winning! ****************** ")
-//     console.log('running at a time every day');
-// });
-//
-// //cron weekday at 5 pm mountain time
-// cron.schedule('0 16 * * 1-5', () => {
-//     run_audit()
-//     console.log("Winning! ****************** ")
-//     console.log('running at a time every day');
-// });
+let debug = function(){
+    let time = new Date()
+    console.log("current time: ",time)
+    console.log("current time: ",time.toLocaleString())
+}
+setInterval(debug,1000)
+
+// weekday run at noon
+cron.schedule('0 12 * * 1-5', () => {
+    tools.getBalance()
+    console.log("Winning! ****************** ")
+    console.log('running at a time every day');
+});
+
+//cron weekday at 5 pm mountain time
+cron.schedule('0 16 * * 1-5', () => {
+    tools.getBalance()
+    console.log("Winning! ****************** ")
+    console.log('running at a time every day');
+});
